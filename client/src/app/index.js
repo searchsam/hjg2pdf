eec9eb08e536c10bdc9be21198533598f00eb4b3
Samuel José Gutiérrez Avilés <samaviles@gmail.com>
2020-08-18 03:07:05 -0600

    cors 2

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -4,7 +4,8 @@ const http = require("http");
 
 document.getElementById("buscar").addEventListener("click", e => {
   let theme = document.getElementById("palabra").value;
-  let url = `https://hjg.com.ar/vocbib/art/${theme}.html`;
+  let url = `https://hjg.com.ar/vocbib/art/${theme.toLowerCase()}.html`;
+
   http
     .get(url, response => {
       const {statusCode} = response;
