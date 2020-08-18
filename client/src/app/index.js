05d17b821411b4dcdfd0c26f8a76da434113deda
Samuel José Gutiérrez Avilés <samaviles@gmail.com>
2020-08-18 02:01:02 -0600

    request

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,5 +1,16 @@
 import "./index.css";
 
+const http = require("http");
+
 document.getElementById("buscar").addEventListener("click", e => {
-  alert(document.getElementById("palabra").value);
+  let theme = document.getElementById("palabra").value;
+  let url = `https://hjg.com.ar/vocbib/art/${theme}.html`;
+  http
+    .get("http://nodejs.org/dist/index.json", response => {
+      const {statusCode} = response;
+      alert(statusCode);
+    })
+    .on("error", e => {
+      console.error(`Got error: ${e.message}`);
+    });
 });
