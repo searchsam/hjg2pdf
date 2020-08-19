840c56b3711f8b0b5a68a426814469510b3d81f1
Samuel José Gutiérrez Avilés <samaviles@gmail.com>
2020-08-18 20:54:26 -0600

    otra libreria

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -1,21 +1,14 @@
+"use strict";
+
 import "./index.css";
 
-const fetch = require("node-fetch");
-const Bluebird = require("bluebird");
+var requestify = require("requestify");
 
-fetch.Promise = Bluebird;
-
-document.getElementById("buscar").addEventListener("click", e => {
-  let theme = document.getElementById("palabra").value;
+// document.getElementById("buscar").addEventListener("click", e => {
+  let theme = 'agua'; //document.getElementById("palabra").value;
   const url = `https://hjg.com.ar/vocbib/art/${theme.toLowerCase()}.html`;
-  let response = fetch(url, {
-    method: "GET",
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "*",
-      "Access-Control-Allow-Headers": "*"
-    }
-  });
 
-  console.log(response);
-});
+  requestify.get(url).then(response => {
+    console.log(response.code);
+  });
+// });
