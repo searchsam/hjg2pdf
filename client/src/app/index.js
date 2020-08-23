efa5a7e1818e1598f5a6ed816ecad80deaa75661
Samuel José Gutiérrez Avilés <samaviles@gmail.com>
2020-08-23 01:51:04 -0600

    Pdf del tema

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -4,54 +4,80 @@ import "./index.css";
 import fs from "fs";
 import Helvetica from "!!raw-loader!pdfkit/js/data/Helvetica.afm";
 
+fs.writeFileSync("data/Helvetica.afm", Helvetica);
+
 const blobStream = require("blob-stream");
+const Entities = require("html-entities").XmlEntities;
 const JSSoup = require("jssoup").default;
 const PDFDocument = require("pdfkit").default;
 
-fs.writeFileSync("data/Helvetica.afm", Helvetica);
-
-const doc = new PDFDocument({size: "LETTER"});
+const doc = new PDFDocument({size: "LETTER", margin: 50});
 const request = new XMLHttpRequest();
 const stream = doc.pipe(blobStream());
-
-const createObjectURL = object => {
-  return window.URL
-    ? window.URL.createObjectURL(object)
-    : window.webkitURL.createObjectURL(object);
-};
-
+const entities = new Entities();
 const a = document.createElement("a");
+const proxy = "https://cors-anywhere.herokuapp.com/";
+
 document.body.appendChild(a);
 a.style = "display: none";
 
-let getTheme = word => {
-  let proxy = "https://cors-anywhere.herokuapp.com/";
-  let url = `https://hjg.com.ar/vocbib/art/${word.toLowerCase()}.html`;
-
+let topicExists = url => {
   request.open("GET", proxy + url, false);
+  request.overrideMimeType("text/xml; charset=iso-8859-1");
   request.send(null);
 
-  if (request.status == 200) {
-    let soup = new JSSoup(request.responseText, false);
-    let theme = soup.find("div", {id: "main"}).text;
-    doc.text(theme);
-    doc.end();
+  if (request.status == 200) return true;
 
-    stream.on("finish", () => {
-      let blob = stream.toBlob("application/pdf");
-      console.log(blob);
-      if (!blob) return;
-      var url = window.URL.createObjectURL(blob);
-      a.href = url;
-      a.download = `${word.toLowerCase()}.pdf`;
-      a.click();
-      window.URL.revokeObjectURL(url);
+  return false;
+};
+
+let getTheme = url => {
+  return new JSSoup(request.responseText, false);
+};
+
+let loadTheme = (word, soup) => {
+  doc
+    .fontSize(20)
+    .text(word.replace(word[0], word[0].toUpperCase()), {align: "center"});
+  doc.fontSize(11);
+  soup
+    .find("div", {id: "main"})
+    .findAll("p")
+    .forEach(p => {
+      doc
+        .moveDown()
+        .text(p.text.replace(/(\r\n|\n|\r)/gm, " "), {
+          align: "justify",
+          ellipsis: true
+        });
     });
-  } else {
-    console.log("Tema no encontrado.");
-  }
+  doc.end();
+};
+
+let downloadDoc = word => {
+  stream.on("finish", () => {
+    let blob = stream.toBlob("application/pdf");
+
+    if (!blob) return;
+
+    let docUrl = window.URL.createObjectURL(blob);
+    a.href = docUrl;
+    a.download = `${word.toLowerCase()}.pdf`;
+    a.click();
+    window.URL.revokeObjectURL(docUrl);
+  });
 };
 
 document.getElementById("buscar").addEventListener("click", e => {
-  getTheme("agua");
+  let word = document.getElementById('palabra').value;
+  let url = `https://hjg.com.ar/vocbib/art/${word.toLowerCase()}.html`;
+
+  if (topicExists(url)) {
+    let soup = getTheme(url);
+    loadTheme(word, soup);
+    downloadDoc(word);
+    window.location.reload(false);
+  } else {
+    console.log("Tema no encontrado.");
+  }
 });
