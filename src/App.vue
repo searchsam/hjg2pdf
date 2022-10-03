a052ebc2d0534a7e417dddf0d4566cc23ec993ea
searchsam <search.sama@gmail.com>
2022-10-03 09:53:18 +0000

    deploy

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,13 @@ export default {
 </script>
 
 <style lang="sass">
+@import url('https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@500&display=swap')
+
 #app
-  font-family: Avenir, Helvetica, Arial, sans-serif
+  font-family: 'Libre Franklin', Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
   text-align: center
-  color: #2c3e50
+  color: #2C3E50
   margin-top: 60px
 </style>
