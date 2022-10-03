8e3cf5b28019e8bbadafe9cada17b4cb3d416be9
searchsam <search.sama@gmail.com>
2022-10-03 02:44:39 -0600

    Añadir carpeta docs

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,10 @@ set -e
 yarn run build
 
 # navigate into the build output directory
-cd dist
+cp dist docs
+
+git add -A
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:searchsam/hjg2pdf.git master:gh-pages
-
-cd -
+git push
